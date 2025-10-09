04f6db062e8be089251aaa935c0370c522d50f07
exy-99 <pratyushtheone@gmail.com>
2025-10-09 12:00:09 +0530

    added separate css and js files

---
+++ script.js
@@ -0,0 +1,17 @@
+const hamburger = document.getElementById("hamburger");
+    const navMenu = document.getElementById("nav-menu");
+    const hamburgerIcon = hamburger.querySelector("i");
+    hamburger.addEventListener("click", () => {
+      navMenu.classList.toggle("active");
+      hamburgerIcon.classList.toggle("fa-bars");
+      hamburgerIcon.classList.toggle("fa-times");
+
+      window.addEventListener("scroll", () => {
+        const nav = document.querySelector("nav");
+        if (window.scrollY > 50) {
+          nav.classList.add("scrolled");
+        } else {
+          nav.classList.remove("scrolled");
+        }
+      });
+    });
