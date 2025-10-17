7b4da182e9d903879d170e5a5945038d9b711e99
exy-99 <pratyushtheone@gmail.com>
2025-10-17 14:36:57 +0530

    added whatsapp chick to chat and added contact form

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 const hamburger = document.getElementById("hamburger");
-    const navMenu = document.getElementById("nav-menu");
-    const hamburgerIcon = hamburger.querySelector("i");
-    hamburger.addEventListener("click", () => {
-      navMenu.classList.toggle("active");
-      hamburgerIcon.classList.toggle("fa-bars");
-      hamburgerIcon.classList.toggle("fa-times");
-
-      window.addEventListener("scroll", () => {
-        const nav = document.querySelector("nav");
-        if (window.scrollY > 50) {
-          nav.classList.add("scrolled");
-        } else {
-          nav.classList.remove("scrolled");
-        }
-      });
-    });
-    
+const navMenu = document.getElementById("nav-menu");
+const hamburgerIcon = hamburger.querySelector("i");
 const backToTop = document.getElementById('backToTop');
-      if (window.scrollY > 300) {
-                  backToTop.classList.add('active');
-              } else {
-                  backToTop.classList.remove('active');
-              }
-        
+const nav = document.querySelector("nav"); 
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+  hamburgerIcon.classList.toggle("fa-bars");
+  hamburgerIcon.classList.toggle("fa-times");
+});
+
+window.addEventListener("scroll", () => {
+  
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+
+  if (window.scrollY > 250) {
+    backToTop.classList.add('active');
+  } else {
+    backToTop.classList.remove('active');
+  }
+});
+
+backToTop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth' 
+  });
+});
+
+
